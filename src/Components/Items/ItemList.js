0fb50b2597814aef967b5ae85d977cf10f964264
emarekica <emarekica@gmail.com>
2022-06-26 19:17:55 +0200

    config: ItemList component

--- src/Components/Items/ItemList.js
+++ src/Components/Items/ItemList.js
@@ -1,9 +1,19 @@
+// outputs user data
+
 import React from "react";
 
 import classes from "./ItemList.modul.css";
 
-const ItemList = () => {
-  return <div></div>;
+// In the ul, go through a list of users that you get from AddUser and output an li
+const ItemList = (props) => {
+  return (
+    <ul className={classes.listItem}>
+      {props.itemsFromInput.map((item) => (
+        // subject to change
+        <li>{item}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default ItemList;
